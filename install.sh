3bce201c23db9e63d296522f48ace5e853888bcb
paperbenni <paperbenni@gmail.com>
2018-11-19 16:49:02 +0100

    script works now

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-curl https://www.vainglorygame.com/quick-guide-install-vg-alpha-windows/ | grep "Download the .exe " > vain.html 
+curl https://www.vainglorygame.com/quick-guide-install-vg-alpha-windows/ | grep "Download the" > vain.html 
 
 grep -o '".*"' vain.html | sed 's/"//g' > link.txt
 rm vain.html
-wget $(cat link.txt)
+wget -O vainglory.exe $(cat link.txt)
+rm link.txt
